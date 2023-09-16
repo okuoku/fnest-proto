7594ddb7983be71c783a1d9e92bbf7fa1b9353b4
okuoku <mjt@cltn.org>
2023-09-16 17:17:30 +0900

    Add RTC offering

--- httpSide/cnt/res.js
+++ httpSide/cnt/res.js
@@ -1,3 +1,13 @@
 addEventListener("load", function(e){
-    window.location.href="https://ce0e-106-178-156-166.ngrok-free.app/cnt/res.html#hoge2"
+    fetch("/con", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({})
+    }).then(function(res){
+        res.text().then(function(str){
+            window.location.href="https://ce0e-106-178-156-166.ngrok-free.app/cnt/res.html#" + str;
+        })
+    });
 });
--- nodeserv.mjs
+++ nodeserv.mjs
@@ -1,13 +1,61 @@
+import nodeDataChannel from "node-datachannel";
+import jose from "node-jose";
 import {serve} from "@hono/node-server";
 import {serveStatic} from "@hono/node-server/serve-static";
 import {Hono} from "hono";
 
+// Temporary key store
+const keystore = jose.JWK.createKeyStore();
+let signer = {}; // Filled in init
+
 const httpApp = new Hono(); // Port: 3040
 const httpsApp = new Hono(); // Port: 3044
 
-httpApp.use("/cnt/*", serveStatic({root: "./httpSide/"}));
-httpsApp.use("/cnt/*", serveStatic({root: "./httpsSide/"}));
+nodeDataChannel.initLogger("Debug");
 
-serve({fetch: httpApp.fetch, port: 3040}, i => console.log("http:",i));
-serve({fetch: httpsApp.fetch, port: 3044}, i => console.log("https:",i));
+function getSdp(){
+    return new Promise((res, rej) => {
+        const iceServers = {
+            iceServers: ["stun:stun.l.google.com:19302"]
+        };
+        const conn = new nodeDataChannel.PeerConnection("conn", iceServers);
+        conn.onLocalDescription((sdp, type) => {
+            console.log("Response(SDP)", sdp);
+            res(sdp);
+        });
 
+        // Activate
+        const dc = conn.createDataChannel("main");
+    });
+}
+
+async function connHandler(c){
+    // FIXME: Need to handle exceptions here..?
+    const obj = await c.req.json();
+    const sdp = await getSdp();
+    const enc = await signer(sdp);
+    return c.text(enc);
+}
+
+async function init(){
+    await keystore.generate("EC", "P-256");
+
+    signer = async function(obj){
+        const res = await jose.JWS.createSign({format: "compact"},
+                                              keystore.all()).
+            update(obj).
+            final();
+        return res;
+    }
+
+    httpApp.post("/con", connHandler);
+
+    httpApp.use("/cnt/*", serveStatic({root: "./httpSide/"}));
+    httpsApp.use("/cnt/*", serveStatic({root: "./httpsSide/"}));
+
+    serve({fetch: httpApp.fetch, port: 3040}, i => console.log("http:",i));
+    serve({fetch: httpsApp.fetch, port: 3044}, i => console.log("https:",i));
+}
+
+
+init();
