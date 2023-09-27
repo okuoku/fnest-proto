0af822dbec6534003666cd95d5948d91b652338c
okuoku <mjt@cltn.org>
2023-09-27 22:22:20 +0900

    test_bandwidth: Adjust buffer logic

--- test_bandwidth_browser.mjs
+++ test_bandwidth_browser.mjs
@@ -1,29 +1,38 @@
 function dc_tester(dc, cb){
     const BUFSIZE = 16 * 1000;
     const LWSIZE = 20;
-    const LOW_WATERMARK = BUFSIZE * LWSIZE;
     const QUEUESIZE = 200;
+    const LOW_WATERMARK = BUFSIZE * LWSIZE;
+    const HIGH_WATERMARK= BUFSIZE * QUEUESIZE;
     const dummybuf = new ArrayBuffer(BUFSIZE);
 
-    function onLW(){
-        const COUNT = QUEUESIZE - LWSIZE;
-        for(let i = 0; i != COUNT; i++){
+    function fill(){
+        while(dc.bufferedAmount < HIGH_WATERMARK){
             dc.send(dummybuf);
         }
+        console.log("Fill done", dc.bufferedAmount);
+    }
+
+    function onLW(){
+        console.log("On LW", dc.bufferedAmount);
+        fill();
     }
 
     function onMsg(ev){
         cb(ev.data);
     }
 
+    function onOpen(ev){
+        // Send initial content
+        fill();
+        console.log("Initial content", dc.bufferedAmount);
+    }
+
     dc.bufferedAmountLowThreshold = LOW_WATERMARK;
     dc.addEventListener("message", onMsg);
     dc.addEventListener("bufferedamountlow", onLW);
+    dc.addEventListener("open", onOpen);
 
-    // Send initial content
-    for(let i = 0; i != QUEUESIZE; i++){
-        dc.send(dummybuf);
-    }
 
 }
 
